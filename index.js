20b2aca94de1236335b558609f41580cc4d9d8fc
cilemite <bedrockgameover@gmail.com>
2024-09-04 01:44:16 -0500

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["🟣 cilemite","💜 cile_cord"];
 
 
 let currentIndex = 0;
